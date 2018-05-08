191488aa124c4ce397716bc3d876595f3765a16e
Joseph Hernandez <josephmar.hernandez@gmail.com>
2018-05-07 23:05:32 -0500

    Initial Commit: Setting up app

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<?php
+	require "php_files/html_structure.php";
+	$html_format = new HtmlFormat();
+	$head_meta = $html_format->meta_head();
+	$head_jquery = $html_format->html_head_js();
+	echo $head_meta;
+	echo $head_jquery;
+?>
+</head>
+<body>
+	<h1>Welcome</h1>
+</body>
+<?php
+	$footer_js = $html_format->html_footer_js();
+	$footer_css = $html_format->html_footer_css();
+	echo $footer_js;
+	echo $footer_css;
+?>
+</html>
---
+++ php_files/html_structure.php
@@ -0,0 +1,34 @@
+<?php
+class HtmlFormat {
+
+	public function meta_head() {
+		return "
+			<meta charset='UTF-8'>
+			<meta name='description' content='Window sales form'>
+			<meta name='keywords' content='Champion, Windows, Form'>
+			<meta name='author' content='Joseph Hernandez'>
+			<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		";
+	}
+
+	public function html_head_js() {
+		return "
+			<script src='dist/js/jquery-3.3.1.min.js'></script>
+			<script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js'></script>
+		";
+	}
+
+	public function html_footer_js(){
+		return "
+			<script src='dist/js/bootstrap.min.js'></script>
+		";
+	}
+
+	public function html_footer_css(){
+		return "
+			<link rel='stylesheet' href='dist/css/bootstrap.min.css'>
+		";
+	}
+
+}
+?>
