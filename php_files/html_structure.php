73ba6de161582fb4b2bd8bedfec73cf333ea7926
Joseph Hernandez <josephmar.hernandez@gmail.com>
2018-05-11 20:48:09 -0500

    Remove angular

--- php_files/html_structure.php
+++ php_files/html_structure.php
@@ -14,7 +14,6 @@ class HtmlFormat {
 	public function html_head_js() {
 		return "
 			<script src='dist/js/jquery-3.3.1.min.js'></script>
-			<script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js'></script>
 		";
 	}
 
